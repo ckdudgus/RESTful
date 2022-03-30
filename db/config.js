868ceadd2650d060bcde2497e0bab0d9ee40f9f3
ckdudgus <c91878074@gmail.com>
2022-03-30 18:05:31 +0900

    비동기처리완료 password는 안올림

---
+++ db/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host : "younghyun1.cy5edtegmfkz.us-east-1.rds.amazonaws.com",
+    database : "cyh_preinterview",
+    user : "admin",
+    password : "",
+    port : 3306,
+    connectionLimit : 66
+}
